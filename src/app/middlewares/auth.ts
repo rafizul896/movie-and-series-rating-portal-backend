2e782ceeb640864c2aba87565c7e1a8927297bd4
rafizul896 <rafizulislam896@gmail.com>
2025-05-01 19:47:33 +0600

    add: Reusable Authentication Middleware

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from 'express';
+import AppError from '../errors/AppError';
+import status from 'http-status';
+import { verifyToken } from '../modules/Auth/auth.utils';
+import config from '../config';
+import { Secret } from 'jsonwebtoken';
+import prisma from '../shared/prisma';
+import { UserStatus } from '@prisma/client';
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token: string = req.headers.authorization!;
+
+      if (!token) {
+        throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
+      }
+
+      const verifyUser = verifyToken(
+        token,
+        config.JWT.JWT_ACCESS_SECRET as Secret,
+      );
+
+      req.user = verifyUser;
+
+      if (roles.length && !roles.includes(verifyUser.role)) {
+        throw new AppError(status.UNAUTHORIZED, 'You are not authorized!');
+      }
+
+      const userData = await prisma.user.findUnique({
+        where: {
+          email: verifyUser.email,
+        },
+      });
+
+      if (userData?.status !== UserStatus.ACTIVE) {
+        throw new AppError(status.UNAUTHORIZED, 'Action not allowed');
+      }
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+export default auth;
