23610d492cd9f520b41625600841c0ec0bf97a0b
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-05-03 21:15:48 +0600

    resolve conflict issues

--- src/app/modules/movie/movie.route.ts
+++ src/app/modules/movie/movie.route.ts
@@ -1,22 +1,22 @@
 import { Router } from 'express';
 import { movieController } from './movie.controller';
 import { MovieValidation } from './movie.validation';
-import validateRequest from '../../utils/validateRequest';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
+import { validationRequest } from '../../middlewares/validationRequest';
 
 const movieRoutes = Router();
 
 movieRoutes.post(
   '/',
   auth(UserRole.ADMIN),
-  validateRequest(MovieValidation.addMovieSchema),
+  validationRequest(MovieValidation.addMovieSchema),
   movieController.addAMovie,
 );
 movieRoutes.get('/', movieController.getAllMovie);
 movieRoutes.get('/:id', movieController.getAMovie);
 movieRoutes.patch('/:id',auth(UserRole.ADMIN),
-validateRequest(MovieValidation.updateMovieSchema), movieController.updateAMovie);
+validationRequest(MovieValidation.updateMovieSchema), movieController.updateAMovie);
 movieRoutes.delete('/soft/:id',auth(UserRole.ADMIN), movieController.deleteAMovie);
 
 export default movieRoutes;
