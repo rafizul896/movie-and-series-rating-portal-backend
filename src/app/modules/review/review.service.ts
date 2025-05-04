57ab8f7c33db251d481eeb90773bea9b57e17979
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-05-04 16:55:19 +0600

    Adds a directive to disable the ESLint rule for unused variables in the file. This may be to prevent linting errors during development or to allow flexibility for future use.

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Review, User, UserRole } from '@prisma/client';
 import prisma from '../../shared/prisma';
 import AppError from '../../error/AppError';
