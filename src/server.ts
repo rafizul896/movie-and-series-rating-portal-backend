799e67a01dcf1dd27ecefc12c11921f603dbbbd4
AKTHER UZ ZAMAN <90123719+opu183059@users.noreply.github.com>
2025-05-06 12:57:21 +0600

    Corrected spelling mistake
    Changed the text file in server.ts file

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './app/config';
 async function main() {
   try {
     app.listen(config.PORT, () => {
-      console.log('App is listening on poet', config.PORT);
+      console.log('App is listening on port', config.PORT);
     });
   } catch (err) {
     console.log(err);
