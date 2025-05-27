209dd9ee33e8c058e18d0743779209c76b00f12a
rafizul896 <rafizulislam896@gmail.com>
2025-05-27 16:46:41 +0600

    update: updateUser service

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -12,7 +12,7 @@ const passwordChangeSchema = z.object({
     oldPassword: z
       .string()
       .min(4, 'Old password must be at least 4 characters long'),
-      newPassword: z
+    newPassword: z
       .string()
       .min(4, 'New password must be at least 4 characters long')
       .refine((val) => val !== '', 'New password cannot be empty'),
